74241a479fe8f7338410b0d18026f87efa6e6f20
habibasaker <habiba.amr.osama@gmail.com>
2025-05-27 23:32:51 +0300

    Update match_case_calculator.py

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,1 +1,13 @@
+num1=input("please enter number 1: ")
+num2=input("please enter number 2: ")
+opp=input("Choose the operation (+, -, *, /): ")
+
+if opp=="+": 
+  print (num1+num2)
+elif opp==-:
+  print (num1-num2) 
+elif opp==*:
+  print (num1*num2)
+else :
+  print (num1/num2)
 
